7c18431eb8d3cad45929c0ea937a8c56c3ac475b
Margot Herail <margot.herail@hotmail.fr>
2023-03-01 18:37:47 +0100

    exo 2 test

--- ex02/ft_print_half_array.js
+++ ex02/ft_print_half_array.js
@@ -1,2 +1,10 @@
 /* Écrire une fonction qui affiche dans la console de développement une case sur deux d’un tableau de reçu en paramètre. */
 
+function CasesAlternees(tableau) {
+    for (let i = 0; i < tableau.length; i += 2) { 
+      console.log(tableau[i]); 
+    }
+  }
+  
+  
+  
