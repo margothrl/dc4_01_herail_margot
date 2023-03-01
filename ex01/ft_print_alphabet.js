5d0befc6ef804e739186c43c70652cd467468f9e
Margot Herail <margot.herail@hotmail.fr>
2023-03-01 18:20:58 +0100

    test exo 1

--- ex01/ft_print_alphabet.js
+++ ex01/ft_print_alphabet.js
@@ -1,2 +1,8 @@
 /* Écrire une fonction qui affiche l’alphabet en majuscule, une string contenant l’alphabet en minuscule sera passée en paramètre la fonction. Chaque lettre devra être stockée dans une cellule du tableau. */
 
+function afficherAlpha(str) {
+    const alphaMin = str.split(''); 
+    const alphaMaj = alphaMin.map(x => String.fromCharCode(x.charCodeAt() - 32));
+    console.log(alphaMaj);
+  }
+    
