31d59924c1a5b64684db19d27f3cfea524c00c47
Margot Herail <margot.herail@hotmail.fr>
2023-03-01 18:57:32 +0100

    exo 04

--- ex01/ft_print_alphabet.js
+++ ex01/ft_print_alphabet.js
@@ -1,8 +1,9 @@
 /* Écrire une fonction qui affiche l’alphabet en majuscule, une string contenant l’alphabet en minuscule sera passée en paramètre la fonction. Chaque lettre devra être stockée dans une cellule du tableau. */
 
-function afficherAlpha(str) {
-    const alphaMin = str.split(''); 
+function afficherAlpha(abc) {
+    const alphaMin = abc.split(''); 
     const alphaMaj = alphaMin.map(x => String.fromCharCode(x.charCodeAt() - 32));
     console.log(alphaMaj);
   }
     
+ 
--- ex03/ft_print_update_array.js
+++ ex03/ft_print_update_array.js
@@ -1,1 +1,8 @@
 /* Écrire une fonction qui reçoit un tableau de chiffre en paramètre, qui ajoute plus un à la valeur de chaque cellule et qui affiche le tableau mis à jour. */
+
+function ajouterUn(tableau) {
+    const nouveauTableau = tableau.map(x => x + 1); 
+    console.log(nouveauTableau); 
+  }
+  
+  
