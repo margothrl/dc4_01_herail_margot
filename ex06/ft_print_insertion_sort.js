82b041bb99072e3f1064971250e7bd7df3df4018
Margot Herail <margot.herail@hotmail.fr>
2023-03-01 20:00:29 +0100

    exo 6

--- ex06/ft_print_insertion_sort.js
+++ ex06/ft_print_insertion_sort.js
@@ -1,1 +1,16 @@
 /* Écrire une fonction qui prend en entrée un tableau d'entiers non trié et qui le trie par insertion. La fonction doit modifier le tableau d'origine et afficher le tableau trié. */
+
+function triInsertion(tableau) {
+    for (let i = 1; i < tableau.length; i++) {
+      const valeurCourante = tableau[i];
+      let j = i - 1;
+      while (j >= 0 && tableau[j] > valeurCourante) {
+        tableau[j + 1] = tableau[j];
+        j--;
+      }
+      tableau[j + 1] = valeurCourante;
+    }
+    console.log(tableau);
+  }
+  
+ 
